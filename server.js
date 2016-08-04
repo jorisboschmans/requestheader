5312a49507ebf56a751679734972c43c297def39
Joris Boschmans <jorisboschmans@hotmail.com>
2016-08-04 12:21:44 +0200

    fourth server test

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var app = require("express")();
 
 app.get("/api/whoami", function(req, res){
+	var sw = req.headers["user-agent"].match(/\(.+\)/)[0];
+	sw = sw.substring(1,sw.length-1);
 	res.end(JSON.stringify({
-		ipaddress: req.ip.match(/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/)[0],
-		language: null,
-		software: null
+		ipaddress: req.headers['x-forwarded-for']/*.match(/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/)[0]*/,
+		language: req.headers["accept-language"].match(/(^.+)(?=\,)/)[0],
+		software: sw
 	}));
 });
 
