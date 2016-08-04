2bb38e9ebdf6f03599c4d98a815c895b0adcdaec
Joris Boschmans <jorisboschmans@hotmail.com>
2016-08-04 11:59:35 +0200

    second server test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var app = require("express")();
 
 app.get("/api/whoami", function(req, res){
 	res.end(JSON.stringify({
-		ipaddress: req.connection.remoteAddress
+		ipaddress: req.connection.remoteAddress.match(/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/)[0]
 	}));
 });
 
